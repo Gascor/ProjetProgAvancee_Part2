b0cf06db7a7042e80e9865b3fd7582d722b56940
Gascor <lucas-dasilvaferreira@hotmail.com>
2024-12-18 16:00:29 +0100

    fix

--- start_worker.sh
+++ start_worker.sh
@@ -6,10 +6,15 @@ BASE_PATH="/bin"
 # Définir le premier port utilisé
 START_PORT=25545
 
-# Boucle pour lancer 12 instances de WorkerSocket
+# Boucle pour ouvrir les ports et lancer 12 instances de WorkerSocket
 for i in {0..11}
 do
     PORT=$(($START_PORT + $i))
+    echo "Configuring firewall for port $PORT"
+    # Ajouter une règle pour permettre le trafic sur le port actuel
+    sudo iptables -A INPUT -p tcp --dport $PORT -j ACCEPT
+    sudo iptables -A OUTPUT -p tcp --sport $PORT -j ACCEPT
+    
     echo "Starting WorkerSocket on port $PORT"
     java -cp $BASE_PATH src.WorkerSocket $PORT &> /dev/null &
     echo "WorkerSocket started on port $PORT"
